5296b2c4c93d0bb02d92e4b79d41b64279bc9798
arfatbegum <arfatbegum90@gmail.com>
2025-08-03 22:24:31 +0300

    user registration functionality done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,66 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    
+    function UserRegistration(Request $request){
+
+        try{
+        $email=$request->input('email');
+        $name=$request->input('name');
+        $mobile=$request->input('mobile');
+        $password=$request->input('password');
+
+
+        User::create([
+         'name'=>$name,
+         'email'=>$email,
+         'mobile'=>$mobile,
+         'password'=>$password
+        ]);
+
+        return response()->json([
+            'status'=>'success',
+            'message'=>'User Registration Successfully!'
+        ]);
+        }
+        catch(Exception $e){
+            return response()->json([
+            'status'=>'failed',
+            'message'=>$e->getMessage()
+        ], 200);
+        };
+        
+    }
+
+    function UserLogin(Request $request){
+
+    }
+
+    function UserLogout(Request $request){
+
+    }
+
+    function SendOTPCode(Request $request){
+
+    }
+
+     function VerifyOTP(Request $request){
+
+    }
+
+    function ResetPassword(Request $request){
+
+    }
+
+    function UserProfile(Request $request){
+
+    }
+
+    function UpdateProfile(Request $request){
+
+    }
 }
